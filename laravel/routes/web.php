5bcc1dc4619e8f1761b44f6f80f798ea330e9068
andreylion.zt@gmail.com <andreylion.zt@gmail.com>
2025-03-15 15:27:05 +0100

    lab2 laravel crud

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,6 +16,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 Route::get('test', [TestController::class, 'test']);
+
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'getProducts'])->name('get_products');
+    Route::get('/{id}', [ProductController::class, 'getProductItem'])->name('get_product_item');
+    Route::post('/', [ProductController::class, 'createProduct'])->name('post_products');
+    Route::put('/{id}', [ProductController::class, 'updateProduct'])->name('update_product');
+    Route::delete('/{id}', [ProductController::class, 'deleteProduct'])->name('delete_product');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
